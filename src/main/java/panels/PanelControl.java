b9c7fd7bebe1a76fc496e53817d4f1adf8296afc
Galuant <104265842+galuant@users.noreply.github.com>
2022-05-12 08:08:09 +0300

    Add files via upload

--- src/main/java/panels/PanelControl.java
+++ src/main/java/panels/PanelControl.java
@@ -149,23 +149,7 @@ public class PanelControl extends GridPanel {
         });
         buttons.add(addPoints);
 
-        // управление
-        Button load = new Button(
-                window, false, backgroundColor, PANEL_PADDING,
-                6, 7, 0, 5, 3, 1, "Загрузить",
-                true, true);
-        load.setOnClick(() -> {
-            PanelRendering.load();
-            cancelTask();
-        });
-        buttons.add(load);
 
-        Button save = new Button(
-                window, false, backgroundColor, PANEL_PADDING,
-                6, 7, 3, 5, 3, 1, "Сохранить",
-                true, true);
-        save.setOnClick(PanelRendering::save);
-        buttons.add(save);
 
         Button clear = new Button(
                 window, false, backgroundColor, PANEL_PADDING,
